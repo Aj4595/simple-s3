e93c508ac969cae6badb62d947a23dd03cf0f9d8
EC2 Default User <ec2-user@ip-172-31-10-250.ap-south-1.compute.internal>
2021-02-22 14:24:38 +0000

    second change

--- s3.sh
+++ s3.sh
@@ -1,16 +1,19 @@
 
 #!/bin/bash
 
-#To Find No. of Buckets in s3 and also creation date
+#To Find Names of Buckets and also creation date
 
 echo "The name of buckets are below"
 echo "---------------"
-aws s3 ls --region us-east-1
-echo "---------------"
+aws s3 ls --region us-east-1 
 
+echo "--Total Count is-- "
+aws s3 ls --region us-east-1 | wc -l
 # To check current files insice buckets
 
-#The Files inside the test323ajay are  
+echo "-----***-------------------***--------"
+
+#Check Status of all buckets 
 echo "The Files and status of files inside test323ajay bucket is below"
 
 aws s3 ls s3://test323ajay --recursive --human-readable --summarize
